ebcac018288c8c4bb819003268fe3ca09c759c44
Pirman Kenneth William <kenneth.pirman@epfl.ch>
2018-07-02 14:02:01 +0200

    add 200 route for probe

--- server/libs/routes/index.js
+++ server/libs/routes/index.js
@@ -6,6 +6,7 @@ import config from "../config";
 const router = express.Router();
 
 export default function generateRoutes(app) {
+  app.use("/", router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/`, router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/search`, elasticSearch(router));
   app.get('*', (req, res) => res.status(404).send('Nothing Here :('));
