9eebc44c5729ad1fd5c82044ca89372a4485e692
Pirman Kenneth William <kenneth.pirman@epfl.ch>
2018-07-02 12:05:48 +0200

    move log messages to message lib

--- server/libs/logger.js
+++ server/libs/logger.js
@@ -1,32 +1,28 @@
-
 import morgan from "morgan";
-import chalk from "chalk";
+import messages from "./messages";
 
-function logger (tokens, req, res) {
-  let color, emoji;
-  if (res.statusCode < 400 ) {
-    emoji = '✅    ';
-    color = chalk.green
-  } else if (res.statusCode >= 400 && res.statusCode < 500) {
-    emoji = '🚫    ';
-    color = chalk.magentaBright
-  } else if (res.statusCode >= 500) {
-    emoji = '🔴    ';
-    color = chalk.red
-  }
-  return color([
-    emoji,
+function logger(tokens, req, res) {
+  let statusString = [
     tokens.method(req, res),
     tokens.url(req, res),
     tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms'
-  ].join(' '))
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms"
+  ].join(" ");
+
+  if (res.statusCode < 400) {
+    return messages.LOG_SUCCESS(statusString);
+  } else if (res.statusCode >= 400 && res.statusCode < 500) {
+    return messages.LOG_NOT_FOUND(statusString);
+  } else if (res.statusCode >= 500) {
+    return messages.LOG_SERVER_ERROR(statusString);
+  }
 }
 
-
-function loggerMiddleware (app) {
+function loggerMiddleware(app) {
   app.use(morgan(logger));
-};
+}
 
 export default loggerMiddleware;
