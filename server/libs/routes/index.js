6d93d35df0b1aa1e0f18a28bbd1c5a90ed2978fb
kenjinp <github@kenny.wtf>
2018-07-27 14:38:58 +0200

    add debugging route

--- server/libs/routes/index.js
+++ server/libs/routes/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import elasticSearch from "./elastic-search";
 import config from "../config";
+import fs from "fs";
 
 const router = express.Router();
 
@@ -9,5 +10,9 @@ export default function generateRoutes(app) {
   app.use("/", router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/`, router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/search`, elasticSearch(router));
+  app.use(`${config.SEARCH_API_BASE_PATH}/search/data-list`, (req, res) => {
+    let folders = fs.readdirSync(config.STATIC_DATA_FOLDER);
+    res.json(folders);
+  });
   app.get('*', (req, res) => res.status(404).send('Nothing Here :('));
 }
