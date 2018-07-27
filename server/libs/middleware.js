a169e2c5aa01314791f45c5acabfa440043cb6c5
kenjinp <github@kenny.wtf>
2018-07-27 15:12:42 +0200

    move static routes

--- server/libs/middleware.js
+++ server/libs/middleware.js
@@ -1,13 +1,10 @@
 import cors from "cors";
 import logger from "./logger";
 import routes from "./routes";
-import express from "express";
-import config from "./config";
 
 function middleware (app) {
   logger(app);
   app.use(cors());
-  app.use(`${config.SEARCH_API_BASE_PATH}/search/data`, express.static(config.STATIC_DATA_FOLDER));
   routes(app);
 };
 
--- server/libs/routes/index.js
+++ server/libs/routes/index.js
@@ -3,14 +3,22 @@ import express from "express";
 import elasticSearch from "./elastic-search";
 import config from "../config";
 import fs from "fs";
+import express from "express";
+import config from "./config";
 
 const router = express.Router();
 
 export default function generateRoutes(app) {
   app.use("/", router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/`, router.get('/', (req, res) => res.sendStatus(200)));
+  app.use(`${config.SEARCH_API_BASE_PATH}/data`, express.static(config.STATIC_DATA_FOLDER));
+  app.use(`${config.SEARCH_API_BASE_PATH}/search/data`, express.static(config.STATIC_DATA_FOLDER));
   app.use(`${config.SEARCH_API_BASE_PATH}/search`, elasticSearch(router));
   app.use(`${config.SEARCH_API_BASE_PATH}/search/data-list`, (req, res) => {
+    let folders = fs.readdirSync(config.STATIC_DATA_FOLDER);
+    res.json(folders);
+  });
+  app.use(`${config.SEARCH_API_BASE_PATH}/data-list`, (req, res) => {
     let folders = fs.readdirSync(config.STATIC_DATA_FOLDER);
     res.json(folders);
   });
