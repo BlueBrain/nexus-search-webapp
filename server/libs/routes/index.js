228969bd932c7f406dc26a5b3382e804a50ecf94
kenjinp <github@kenny.wtf>
2018-09-18 14:28:58 +0200

    debug staging fake json path

--- server/libs/routes/index.js
+++ server/libs/routes/index.js
@@ -11,6 +11,7 @@ export default function generateRoutes(app) {
   app.use("/", router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/`, router.get('/', (req, res) => res.sendStatus(200)));
   app.use(`${config.SEARCH_API_BASE_PATH}/search/data/trace`, (req, res) => res.json(testTrace));
+  app.use(`${config.SEARCH_API_BASE_PATH}/data/trace`, (req, res) => res.json(testTrace));
   app.use(`${config.SEARCH_API_BASE_PATH}/search`, elasticSearch(router));
   app.get('*', (req, res) => res.status(404).send('Nothing Here :('));
 }
