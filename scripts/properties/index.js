7fed20d7357106da57a0a3719e3a547c21cc925c
kenjinp <github@kenny.wtf>
2018-11-19 12:32:53 +0100

    fix mapping properties so they can be used after a server build, prepare rpc resources

--- scripts/properties/index.js
+++ scripts/properties/index.js
@@ -1,12 +1,23 @@
-import fs from "fs";
+import type from "./@type";
+import brainLocation from "./brainLocation";
+import cellType from "./cellType";
+import channelType from "./channelType";
+import contribution from "./contribution";
+import dataSource from "./dataSource";
+import dataType from "./dataType";
+import license from "./license";
+import studyType from "./studyType";
+import subject from "./subject";
 
-let properties = {};
-
-fs.readdirSync(__dirname + '/').forEach(file => {
-  if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    let name = file.replace('.js', '');
-    properties[name] = require('./' + file).default;
-  }
-});
-
-export default properties;
+export default {
+  "@type": type,
+  brainLocation,
+  cellType,
+  channelType,
+  contribution,
+  dataSource,
+  dataType,
+  license,
+  studyType,
+  subject
+}
